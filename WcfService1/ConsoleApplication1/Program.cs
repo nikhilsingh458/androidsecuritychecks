b2b8451cadad524c5f79308ce640ed35c9de2065
Nikhil <Nikhil.singh@neudesic.com>
2016-02-21 21:33:52 +0530

    WCF Service

---
+++ WcfService1/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.CalculatorClient svr = new ServiceReference1.CalculatorClient();
+            double sum=svr.AddNumbers(10.25, 100.25);
+        }
+    }
+}
---
+++ WcfService1/WcfService1/Calculator.svc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Calculator" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Calculator.svc or Calculator.svc.cs at the Solution Explorer and start debugging.
+    public class Calculator : ICalculator
+    {
+        
+        public double AddNumbers(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public double SubtractNumbers(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
+        public double MultiplyNumbers(double number1, double number2)
+        {
+            return number2 * number1;
+        }
+    }
+}
